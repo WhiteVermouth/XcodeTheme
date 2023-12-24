11e372db6b0b5a1fb39aa1c8fad38e20a1144b6c
Zihan Ma <zihan.ma@protonmail.com>
2023-12-24 23:02:58 +0800

    👷 Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,28 +51,12 @@ tasks {
         val description = """
             <div>
               <p>Xcode Light &amp; Dark themes for JetBrains</p>
-              <br />
-              <p>
-                <img
-                  src="https://raw.githubusercontent.com/WhiteVermouth/XcodeTheme/master/assets/screenshot.png"
-                  width="600"
-                />
-              </p>
-              <h2>Color Palette</h2>
-              <p>
-                <img
-                  alt="Color Palette"
-                  src="https://raw.githubusercontent.com/WhiteVermouth/XcodeTheme/master/assets/color-palette.png"
-                  width="600"
-                />
-              </p>
               <h2>Licence</h2>
               <p>
-                <a
-                  href="https://raw.githubusercontent.com/WhiteVermouth/XcodeTheme/master/LICENSE"
-                  >Apache-2.0 Licence</a
-                >
+                <a href="https://raw.githubusercontent.com/WhiteVermouth/XcodeTheme/master/LICENSE">Apache-2.0 Licence</a>
               </p>
+              <h2>Donation</h2>
+              <p>If you like this plugin, you can <a href="https://www.buymeacoffee.com/nszihan">buy me a cup of coffee</a>. Thank you!</p>
             </div>
         """.trimIndent()
 
